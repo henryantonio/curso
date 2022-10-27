ae3e8c3b58f1dea1c91937af120e55cff3da05b6
Henry Antonio <henry100607@gmail.com>
2022-10-26 17:00:50 -0700

    Segundo commit desde el curso

---
+++ ruby/comentarios.rb
@@ -0,0 +1,8 @@
+#Esto es un comentario de una sola línea.
+
+=begin
+    Esto es un comentario 
+    de varias lineas.
+=end
+
+nombre = 'Antonio' #Esto es una variable nombre.
---
+++ ruby/numeros.rb
@@ -0,0 +1,26 @@
+puts 1+2
+puts 10-11
+puts 2*3
+puts 3/2 #muestra sólo la parte entera porque ambos valores son enteros, no decimales.
+puts 3/2.0
+
+numero = 2
+puts 5/numero.to_f
+
+puts 'Ejemplo de ciclo'
+3.times do |numero|
+    puts numero
+end
+puts 'Fin de ciclo'
+
+puts 'Muestra la clase de los objetos'
+puts 16.class
+puts 16.0.class
+
+puts 'Interpolación de cadenas:'
+nombre = 'Henry Antonio'
+puts 'Hola ' + nombre
+puts "Hola #{nombre}"
+
+puts 'Insertando comilla simple a través de la pleca invertida'
+puts 'Comilla simple '
