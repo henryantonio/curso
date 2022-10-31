6eb2fac54ec93455b853e6802114e0f11852934d
Henry Antonio <henry100607@gmail.com>
2022-10-31 16:58:05 -0700

    Reiniciar proyecto al del profesor

--- depot/db/migrate/20221028223440_create_products.rb
+++ depot/db/migrate/20221028223440_create_products.rb
@@ -2,9 +2,9 @@ class CreateProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :products do |t|
       t.string :title, null: false, limit: 250
-      t.text :description, null:false
+      t.text :description, null: false
       t.string :image_url
-      t.decimal :price, null:false
+      t.decimal :price, null: false
 
       t.timestamps
     end
